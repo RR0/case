f33161d60f462f689297b35f0da5952a3e060ec4
javarome <javarome@gmail.com>
2025-05-31 19:19:35 +0200

    build & typing

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-// vite.config.js
 import { defineConfig } from "vite"
+import path from "path"
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 export default defineConfig({
   build: {
     lib: {
-      entry: "src/index.js",
+      entry: {
+        "index": path.resolve(__dirname, "lib/index.js"),
+      },
       formats: ["es"]
     }
   }
